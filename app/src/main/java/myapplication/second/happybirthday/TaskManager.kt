b8d13262866779a72c2fe7adda0553e4a37eb70b
jiyun <jiyun_99@naver.com>
2023-02-09 19:36:06 +0900

    Compose 기본사항 - TaskManager(sampleCode)

--- app/src/main/java/myapplication/second/happybirthday/TaskManager.kt
+++ app/src/main/java/myapplication/second/happybirthday/TaskManager.kt
@@ -1,6 +1,5 @@
 package myapplication.second.happybirthday
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
