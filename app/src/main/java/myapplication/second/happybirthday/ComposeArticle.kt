4621fd0e7f62819d9abe7ce5cf967ec2bda70582
jiyun <jiyun_99@naver.com>
2023-02-09 19:02:56 +0900

    Compose 기본사항 - ComposeArticle(myCode)

---
+++ app/src/main/java/myapplication/second/happybirthday/ComposeArticle.kt
@@ -0,0 +1,88 @@
+package myapplication.second.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import myapplication.second.happybirthday.ui.theme.HappyBirthdayTheme
+
+class ComposeArticle : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DrawViewWithTextAndImage(
+                        getString(R.string.title),
+                        getString(R.string.compose_define),
+                        getString(R.string.description)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DrawViewWithTextAndImage(title: String, define: String, description: String) {
+    Column {
+        val image = painterResource(R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            null,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = define,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp, top = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticlePreview() {
+    HappyBirthdayTheme {
+        DrawViewWithTextAndImage(
+            "Jetpack Compose tutorial",
+            "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+        )
+    }
+}
