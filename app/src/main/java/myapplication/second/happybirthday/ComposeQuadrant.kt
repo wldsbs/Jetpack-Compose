d4cf9b1ad9452eb5229090f32d14377f2f674abc
jiyun <jiyun_99@naver.com>
2023-02-09 20:26:33 +0900

    Compose 기본사항 - ComposeQuadrant(myCode) - 내가 구현한 건 Compose 카드 하나... - 사분면 구현해야함

---
+++ app/src/main/java/myapplication/second/happybirthday/ComposeQuadrant.kt
@@ -0,0 +1,66 @@
+package myapplication.second.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import myapplication.second.happybirthday.ui.theme.HappyBirthdayTheme
+
+class ComposeQuadrant : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Drawing(name: String, description: String, color: Color) {
+    Column(
+        modifier = Modifier
+            .background(color)
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = name,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HappyBirthdayTheme {
+        Drawing(name = "text composable", description = "hello", Color.Green)
+    }
+}
